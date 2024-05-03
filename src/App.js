a7613911a72907231224285d63ebd450d270d06d
Shaa1345 <Shaa1345@icloud.com>
2024-05-03 00:31:51 -0500

    Created the bare bones of a react project.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TitleDisplay from './components/Title'
+
+function App() {
+  return (
+    <div className="App">
+      <TitleDisplay/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Title() {
+  return (
+    <div className = "Title">
+      Recipe Books
+    </div>
+  )
+}
+
+export default Title
